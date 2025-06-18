3834ae53856dab03a5ac941113e7b32609b76547
Ben Sauerländer <ben.sauerlaender@student.htw-berlin.de>
2025-06-18 11:29:33 +0200

    Remove Example model and its import from API routes

--- src/models/Example.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const exampleSchema = new mongoose.Schema({
-  name: String,
-  value: Number
-});
-
-module.exports = mongoose.model('Example', exampleSchema); 
